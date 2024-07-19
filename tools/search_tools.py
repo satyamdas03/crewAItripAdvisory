834946bc68725c61c47eb3339e797a877c12e6a4
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-19 16:06:37 +0530

    search_tools ready

--- tools/search_tools.py
+++ tools/search_tools.py
@@ -26,3 +26,20 @@ class SearchTools():
             return "Sorry, I couldn't find anything about that, there could be an error with you serper api"
         else:
             results = response.json()['organic']
+            string = []
+            for result in results[:top_result_to_return]:
+                try:
+                    string.append('\n'.join([
+                        f"Title: {result['title']}", f"Link: {result['link']}",
+                        f"Snippet: {result['snippet']}", "\n--------"
+                    ]))
+                except KeyError:
+                    next
+
+            return '\n'.join(string)
+
+## over here we are passing along the payload which is going to be our query, so what we are doing is, we are making a post request to a certain
+## .. endpoint, with a certain header, and want to query for certain data.
+## .. for the response, and we are going to do some generic validation, that we are getting a response, yes or no.
+## .. if we are getting a response, we are pulling out the information that was gathered for us, then we will start building a repository of 
+## .. data that was passed back to the cell, we are going to grab the title , link and the snippet of what the search result said
