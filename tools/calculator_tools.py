ec50acd316c6f2e0bc50ae7a869a813d83af2a85
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-19 14:57:47 +0530

    commented the under the hood code for the calculator_tools.py code

--- tools/calculator_tools.py
+++ tools/calculator_tools.py
@@ -11,30 +11,33 @@
 #             return eval(operation)
 #         except SyntaxError:
 #             return "Error: Invalid syntax in mathematical expression"
+
+
+
         
-        
-from pydantic import BaseModel, Field
-from langchain.tools import tool
+### under the hood, this is the code functionality which is making the above template work
+# from pydantic import BaseModel, Field
+# from langchain.tools import tool
 
-#define a Pydantic model for the tool's input parameters
-class CalculationInput(BaseModel):
-    operation: str = Field(..., description = "The mathematical operation to perform")
-    factor : float = Field(..., description = "A factor by which to multiply the result of the operation" )
+# #define a Pydantic model for the tool's input parameters
+# class CalculationInput(BaseModel):
+#     operation: str = Field(..., description = "The mathematical operation to perform")
+#     factor : float = Field(..., description = "A factor by which to multiply the result of the operation" )
 
-#use the tool decorator with the args_schema parameter pointing to the Pydantic model
-@tool("perform_calculation", args_schema=CalculationInput, return_direct = True)
-def perform_calculation(operation : str, factor : float) -> str:
-    """
-    Performs a specified mathematical operation and multiples the result to the given factor.
-    Parameters:
-    -operation(str): A String representing a mathematical operation(eg., "10+5")
-    -factor(float) : A factor by which to multiply the result to the operation
-    Returns :
-    - A String representation of the calculation result.
-    """
-    # Perform the calculation 
-    result = eval(operation)*factor
+# #use the tool decorator with the args_schema parameter pointing to the Pydantic model
+# @tool("perform_calculation", args_schema=CalculationInput, return_direct = True)
+# def perform_calculation(operation : str, factor : float) -> str:
+#     """
+#     Performs a specified mathematical operation and multiples the result to the given factor.
+#     Parameters:
+#     -operation(str): A String representing a mathematical operation(eg., "10+5")
+#     -factor(float) : A factor by which to multiply the result to the operation
+#     Returns :
+#     - A String representation of the calculation result.
+#     """
+#     # Perform the calculation 
+#     result = eval(operation)*factor
 
-    #return the result as a string 
-    return f"The result of {operation} multiplied by the {factor} is {result}"
+#     #return the result as a string 
+#     return f"The result of {operation} multiplied by the {factor} is {result}"
 
